b720b8af1fd152f7e76ccb41b32f3ecd1e8ff43b
Lucas Daniel <118680767+lucaasd@users.noreply.github.com>
2025-08-23 13:34:26 -0300

    feat(lexer): Add token list

--- src/compiler/lexer/toxens.hpp
+++ src/compiler/lexer/toxens.hpp
@@ -1,3 +1,26 @@
-enum class TokenType {
+#ifndef TOKENS
+#define TOKENS
 
+enum class TokenType {
+  IDENTIFIER,
+  CLASS,
+  STRUCT,
+  FUNCTION,
+  VAR,
+  CHAR,
+  CHAR_LITERAL,
+  CHAR_ARRAY_LITERAL,
+  FLOAT_LITERAL,
+  FLOAT,
+  INT_LITERAL,
+  INT,
+  ARRAY,
+  POINTER,
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  MODULUS,
 };
+
+#endif
