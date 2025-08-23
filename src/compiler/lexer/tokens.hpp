556f6100cc0cb463f73c61561c869d3e92c7013d
Lucas Daniel <118680767+lucaasd@users.noreply.github.com>
2025-08-23 13:45:15 -0300

    feat(lexer): Add Token struct

--- src/compiler/lexer/tokens.hpp
+++ src/compiler/lexer/tokens.hpp
@@ -1,26 +1,33 @@
 #ifndef TOKENS
 #define TOKENS
 
+#include <string>
+
 enum class TokenType {
-  IDENTIFIER,
-  CLASS,
-  STRUCT,
-  FUNCTION,
-  VAR,
-  CHAR,
-  CHAR_LITERAL,
-  CHAR_ARRAY_LITERAL,
-  FLOAT_LITERAL,
-  FLOAT,
-  INT_LITERAL,
-  INT,
-  ARRAY,
-  POINTER,
-  ADD,
-  SUB,
-  MUL,
-  DIV,
-  MODULUS,
+    IDENTIFIER,
+    CLASS,
+    STRUCT,
+    FUNCTION,
+    VAR,
+    CHAR,
+    CHAR_LITERAL,
+    CHAR_ARRAY_LITERAL,
+    FLOAT_LITERAL,
+    FLOAT,
+    INT_LITERAL,
+    INT,
+    ARRAY,
+    POINTER,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    MODULUS,
+};
+
+struct Token {
+    std::string text;
+    TokenType value;
 };
 
 #endif
